03174d3b7947613ecb5eb4d269b72fdb7f9fb4e6
anorisno <anorisno@anorisno.anorisno>
2024-03-12 18:45:26 +0300

    init commit

---
+++ src/main/java/com/anorisno/appDataTool/AppDataToolApplication.java
@@ -0,0 +1,13 @@
+package com.anorisno.appDataTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDataToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDataToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anorisno/appDataTool/AppDataToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anorisno.appDataTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDataToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
