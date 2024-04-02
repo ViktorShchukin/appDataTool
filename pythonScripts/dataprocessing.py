00b3a96a0cbf41de88d62e8a7234f910d7affc93
anorisno <anorisno@anorisno.anorisno>
2024-04-02 13:14:21 +0300

    some work with script

--- pythonScripts/dataprocessing.py
+++ pythonScripts/dataprocessing.py
@@ -11,3 +11,61 @@ def moving_average_3(coll: list):
 	correct.append(0)
 	return correct
 
+class KalmanFilter:
+	"""docstring for KalmanFilter"""
+	def __init__(self, q, r, f, h):
+		self.q = q
+		self.r = r
+		self.f = f
+		self.h = h
+		self.firstValue = True
+
+	def set_state(self, state, covariance):
+		self.state = state
+		self.covariance = covariance
+
+	def correct(self, data):
+		if (self.firstValue):
+			self.set_state(data, 0.1)
+			self.firstValue = False
+			return self.state
+
+		# time update - prediction
+		# predicted state
+		x0 = self.f * self.state
+		# predicted covariance
+		p0 = self.f * self.covariance * self.f + self.q
+
+		# measurement update - correction
+		K = self.h * p0 /(self.h * p0 * self.h + self.r);
+		self.state = x0 + K*(data - self.h * x0);
+		self.covariance = (1 - K* self.h)* p0;
+		return self.state;
+
+def get_velocity(coll_acc: list, coll_timestamp: list):
+	i = 0
+	velocity = list()
+	velocity.append(0)
+	while i < (len(coll_acc)-1):
+		i += 1
+		velocity_delta = (coll_acc[i] + coll_acc[i-1]) / 2 * (coll_timestamp[i] - coll_timestamp[i-1]) / 1e9
+		velocity.append(velocity[i-1] + velocity_delta)
+	return velocity
+
+def get_distance(coll_acc: list, coll_timestamp: list):
+	i = 0
+	velocity = list()
+	velocity.append(0)
+	while i < (len(coll_acc)-1):
+		i += 1
+		velocity_delta = (coll_acc[i] + coll_acc[i-1]) / 2 * (coll_timestamp[i] - coll_timestamp[i-1]) / 1e9
+		velocity.append(velocity[i-1] + velocity_delta)
+	return velocity
+
+
+
+if __name__ == '__main__':
+	test = KalmanFilter(2.0, 15.0, 1.0, 1.0)
+	for i in range(10):
+		print(test.correct(i))
+		
--- pythonScripts/printDataOfExperiment.py
+++ pythonScripts/printDataOfExperiment.py
@@ -20,22 +20,38 @@ def print_fig(x: list, y: list, label_new: str, title: str):
 	plt.show()
 
 if __name__ == '__main__':
-	accx, accy, accz, timestamp = ad.get_data(name = "row_data_100_1")
-	accy_correct = dp.moving_average_3(accy)
+	for j in range(1,7):
+		accx, accy, accz, timestamp = ad.get_data(name = "row_data_100_2_" + str(j))
+		accy_correct = list()
+		kalman_filter = dp.KalmanFilter(q = 0.1, r = 0.5, f = 1.0, h = 1.0)
+		for i in range(len(accy)):
+			accy_correct.append(kalman_filter.correct(accy[i]))
+		velocity_y = dp.get_velocity(accy_correct, timestamp)
+		distance_y = dp.get_distance(velocity_y, timestamp)
 
-	# print_fig(x = timestamp, y = accy_correct, label_new = "accy_correct", title = "accy 100cm on floor\ncorrect samle: 1")
 
-	fig, ax = plt.subplots()
-	ax.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
-	ax.set_xlabel("timestamp[ns]")
-	ax.set_ylabel("acceleration[m/s^2]")
+		# print_fig(x = timestamp, y = accy_correct, label_new = "accy_correct", title = "accy 100cm on floor\ncorrect samle: 1")
 
-	ax.plot(timestamp, accy, label = "accy")
-	ax.plot(timestamp, accy_correct, label = "accy_correct")
+		fig, ax = plt.subplots()
+		ax.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
+		ax.set_xlabel("timestamp[ns]")
+		ax.set_ylabel("acceleration[m/s^2]")
+		# xmin, xmax = -5, 5
+		ax.set_ylim(top = 2, bottom = -1.5)
 
-	ax.legend()
-	ax.set_title("accy + correct 100cm on floor\nsamle: 1")
-	plt.show()
+		# ax.plot(timestamp, accy, label = "accy", marker = "o")
+		ax.plot(timestamp, accy_correct, label = "accy_correct", marker = "d")
+		ax.plot(timestamp, distance_y, label = "distance", color = "r")
+
+		ax2 = ax.twinx()
+		ax2.plot(timestamp, velocity_y, label = "velocity_y", marker = "*", color = "k")
+		ax2.set_ylabel("velocity[m/s]")
+		ax2.set_ylim(top = 2, bottom = -1.5)
+
+		ax.legend()
+		ax2.legend()
+		ax.set_title("accy + correct 100cm on floor\nsamle: 1")
+		plt.show()
 
 
 
